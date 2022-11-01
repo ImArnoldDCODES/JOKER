53a1f8d80390e9f7b6a08392fc2f1373019c78d7
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-10-31 21:20:46 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css'
+
 
 function App() {
+  const [data, setData] = useState({})
+  const [make, setMake] = useState(false)
+
+  // useEffect(() => {
+  //   axios.get('https://v2.jokeapi.dev/joke/Any?safe-mode')
+  //   .then(res => setData(res.data))
+  // }, []);
+  // console.log(data)
+
+  const control = () => {
+    axios.get('https://v2.jokeapi.dev/joke/Any?safe-mode')
+      .then(res => setData(res.data))
+      setMake(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className='text-[3rem]'>Jokes</h1>
+      </div>
+
+      <div className='mt-40'>
+        <h2>{data.setup}</h2>
+        <h3>{data.delivery}</h3>
+      </div>
+
+      <button onClick={control} className={make ? 'hidden' : 'mt-10'}>SEE</button>
+      <button onClick={control} className={make ? 'mt-10 bg-[#749128] p-2' : 'hidden'}>NEXT</button>
     </div>
   );
 }
